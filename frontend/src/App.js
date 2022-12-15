b6f92879ad821941da02797222eeeb97ad9277de
Prisca <prisca.castanier@gmail.com>
2022-12-16 00:49:12 +0100

    display character component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Characters from "./Components/Characters";
 
 function App() {
   return (
     <div className="App">
-      <p>test</p>
+      <Characters />
     </div>
   );
 }
