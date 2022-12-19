a294c90d97095c1a09cd4c89ac29fad2e9553300
Prisca C <prisca.castanier@gmail.com>
2022-12-19 12:24:49 +0100

    added logic to handle set of class property to character

--- backend/Routes/characters.php
+++ backend/Routes/characters.php
@@ -9,10 +9,19 @@ $app->setBasePath('/rolegame_project/backend/api');
 $app->map(['GET', 'POST'], '/characters/', function (Request $request, Response $response, $args) {
     $requestMethod = $request->getMethod();
     $character = new Characters();
-    if ($requestMethod == 'POST') {
+    $class = new ClassCharacter();
+    /*if ($requestMethod == 'POST') {
         $data = $request->getParsedBody();
         trigger_error("Data: " . json_encode($data),E_USER_WARNING );
         $character->createCharacter($data['name'], $data['class'], $data['hp'], $data['ability'], $data['strength']);
+    }*/
+    if ($requestMethod == 'POST') {
+        $data = $request->getParsedBody();
+        trigger_error("Data: " . json_encode($data),E_USER_WARNING );
+        $getClass = $class->displaySpecificClass($data['class']); // Needed to set class property to character
+        if($getClass){
+            $character->createCharacter($data['name'], $data['class'], $getClass[0]['hp'], $getClass[0]['ability'], $getClass[0]['strength']);
+        }
     }
     $response->getBody()->write(json_encode($character->displayAllCharacters()));
     return $response;
@@ -21,9 +30,13 @@ $app->map(['GET', 'POST'], '/characters/', function (Request $request, Response 
 $app->map(['GET', 'PUT', 'DELETE'], '/characters/{id}', function (Request $request, Response $response, $args) {
     $requestMethod = $request->getMethod();
     $character = new Characters();
+    $class = new ClassCharacter();
     if ($requestMethod == 'PUT') {
         $data = $request->getParsedBody();
-        $character->modifyCharacter($args['id'], $data['name'], $data['class'], $data['hp'], $data['ability'], $data['strength']);
+        $getClass = $class->displaySpecificClass($data['class']); // Needed to set class property to character
+        if($getClass){
+            $character->modifyCharacter($args['id'], $data['name'], $data['class'], $getClass[0]['hp'], $getClass[0]['ability'], $getClass[0]['strength']);
+        }
     } elseif ($requestMethod == 'DELETE') {
         $character->deleteCharacter($args['id']);
     }
